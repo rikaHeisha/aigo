f69a29f5f084db326a3f602fd5ed7a45dd3cd4e8
Rishi Menon <rishimenon212@gmail.com>
2024-09-29 14:42:42 -0400

    tools

--- research/tools/setup.sh
+++ research/tools/setup.sh
@@ -1,12 +1,10 @@
 # Call source tools/setup.sh
 
-if [ -n "${PROJECT_ROOT_DIR+x}" ]; then
-    # We already ran setup.sh so just return
-    echo "Already ran setup.sh. Skipping..."
-    return
-fi
+# if [ -n "${PROJECT_ROOT_DIR}" ]; then
+#     echo "Already ran setup.sh. Skipping..."
+# fi
 
-export PROJECT_ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )../" &> /dev/null && pwd )
+export PROJECT_ROOT_DIR=$( cd "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 export PYTHONPATH="$PROJECT_ROOT_DIR"
 
 echo "Finished setting up"
