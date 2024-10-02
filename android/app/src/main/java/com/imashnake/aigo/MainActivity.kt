f63d3838ec6c11f218928353751312c1a92310e7
imashnake0 <krpaluru@uwaterloo.ca>
2024-10-02 01:38:48 -0400

    Added buttons and camera permission

--- android/app/src/main/java/com/imashnake/aigo/MainActivity.kt
+++ android/app/src/main/java/com/imashnake/aigo/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.imashnake.aigo.ui.theme.AigoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,8 +32,8 @@ class MainActivity : ComponentActivity() {
             AigoTheme {
                 MainScreen(
                     modifier = Modifier
-                         .fillMaxSize()
-                         .background(MaterialTheme.colorScheme.background)
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
                 )
             }
         }
@@ -36,9 +46,35 @@ fun MainScreen(modifier: Modifier = Modifier) {
         modifier = modifier,
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "aigo",
-            color = MaterialTheme.colorScheme.onBackground
-        )
+        Column {
+            AigoIconTextButton(R.drawable.photo_camera, "Take Picture")
+            AigoIconTextButton(R.drawable.round_image, "Pick Image")
+        }
+    }
+}
+
+@Composable
+fun AigoIconTextButton(
+    @DrawableRes drawable: Int,
+    text: String,
+    modifier: Modifier = Modifier,
+    contentDescription: String? = null,
+    onClick: () -> Unit = {},
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.wrapContentSize()
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(drawable),
+                contentDescription = contentDescription,
+            )
+            Text(text)
+        }
     }
 }
