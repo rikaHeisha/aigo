9373ddf7f71dcdbabea012b7a367fe79a0d3a4cd
imashnake0 <krpaluru@uwaterloo.ca>
2024-10-07 02:03:37 -0400

    Cleanup

--- android/app/src/main/java/com/imashnake/aigo/ui/features/CameraOverlay.kt
+++ android/app/src/main/java/com/imashnake/aigo/ui/features/CameraOverlay.kt
@@ -1,17 +1,23 @@
 package com.imashnake.aigo.ui.features
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import kotlinx.serialization.Serializable
 
 @Composable
 fun CameraOverlay(modifier: Modifier = Modifier) {
-    Text(
-        text = "Camera Permissions granted! Show the camera here right now.",
-        color = MaterialTheme.colorScheme.onBackground,
-    )
+    Box(modifier.fillMaxSize()) {
+        Text(
+            text = "Camera Permissions granted! Show the camera here right now.",
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.align(Alignment.Center),
+        )
+    }
 }
 
 @Serializable
